1b5d67e3dbb3441316145f0638736072fc99f98d
kdg <donggri019@gmail.com>
2021-03-11 23:23:49 +0900

    BFS

--- Algorithm/src/Graph/BFS.java
+++ Algorithm/src/Graph/BFS.java
@@ -12,45 +12,36 @@ public class BFS {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// 그래프 초기화
-        for (int i = 0; i < 9; i++) {
-            graph.add(new ArrayList<Integer>());
-        }
-
-        // 노드 1에 연결된 노드 정보 저장 
-        graph.get(1).add(2);
-        graph.get(1).add(3);
-        graph.get(1).add(8);
+		/*
+		 * for (int i = 0; i < 9; i++) { graph.add(new ArrayList<Integer>()); }
+		 * 
+		 * // 노드 1에 연결된 노드 정보 저장 graph.get(1).add(2); graph.get(1).add(3);
+		 * graph.get(1).add(8);
+		 * 
+		 * // 노드 2에 연결된 노드 정보 저장 graph.get(2).add(1); graph.get(2).add(7);
+		 * 
+		 * // 노드 3에 연결된 노드 정보 저장 graph.get(3).add(1); graph.get(3).add(4);
+		 * graph.get(3).add(5);
+		 * 
+		 * // 노드 4에 연결된 노드 정보 저장 graph.get(4).add(3); graph.get(4).add(5);
+		 * 
+		 * // 노드 5에 연결된 노드 정보 저장 graph.get(5).add(3); graph.get(5).add(4);
+		 * 
+		 * // 노드 6에 연결된 노드 정보 저장 graph.get(6).add(7);
+		 * 
+		 * // 노드 7에 연결된 노드 정보 저장 graph.get(7).add(2); graph.get(7).add(6);
+		 * graph.get(7).add(8);
+		 * 
+		 * // 노드 8에 연결된 노드 정보 저장 graph.get(8).add(1); graph.get(8).add(7);
+		 * 
+		 * bfs(1);
+		 */
         
-        // 노드 2에 연결된 노드 정보 저장 
-        graph.get(2).add(1);
-        graph.get(2).add(7);
-        
-        // 노드 3에 연결된 노드 정보 저장 
-        graph.get(3).add(1);
-        graph.get(3).add(4);
-        graph.get(3).add(5);
-        
-        // 노드 4에 연결된 노드 정보 저장 
-        graph.get(4).add(3);
-        graph.get(4).add(5);
-        
-        // 노드 5에 연결된 노드 정보 저장 
-        graph.get(5).add(3);
-        graph.get(5).add(4);
-        
-        // 노드 6에 연결된 노드 정보 저장 
-        graph.get(6).add(7);
-        
-        // 노드 7에 연결된 노드 정보 저장 
-        graph.get(7).add(2);
-        graph.get(7).add(6);
-        graph.get(7).add(8);
-        
-        // 노드 8에 연결된 노드 정보 저장 
-        graph.get(8).add(1);
-        graph.get(8).add(7);
-
-        bfs(1);
+        long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+		        
+		long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+		long secDiffTime = (afterTime - beforeTime); //두 시간에 차 계산
+		System.out.println("수행 속도(ms) : "+secDiffTime);
 	}
 
 	public static void bfs(int start) {
